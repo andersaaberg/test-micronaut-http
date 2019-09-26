559cf6f0e7a0596a9694847cf33f0986552ea9d4
Anders Aaberg <aaa@nine.dk>
2019-09-26 19:51:49 +0200

    Workaround to manually instantiate Micronaut's low-level http-client in Grails 4.0.0, but still get configuration from application.yml

--- grails-app/controllers/test/micronaut/http/LowLevelClientController.groovy
+++ grails-app/controllers/test/micronaut/http/LowLevelClientController.groovy
@@ -1,15 +1,22 @@
 package test.micronaut.http
 
+import io.micronaut.http.client.DefaultHttpClient
+import io.micronaut.http.client.DefaultHttpClientConfiguration
 import io.micronaut.http.client.RxHttpClient
-import io.micronaut.http.client.annotation.Client
-
+import javax.annotation.PostConstruct
 import javax.inject.Inject
 
 class LowLevelClientController {
 
     @Inject
-    @Client('http://www.google.com')
+    DefaultHttpClientConfiguration defaultHttpClientConfiguration
+
     RxHttpClient rxHttpClient
+
+    @PostConstruct
+    void initHttpClient() {
+        rxHttpClient = new DefaultHttpClient(new URL('http://google.com'), defaultHttpClientConfiguration)
+    }
 
     def index() {
         render rxHttpClient.retrieve('/search?q=hello').blockingSingle()
